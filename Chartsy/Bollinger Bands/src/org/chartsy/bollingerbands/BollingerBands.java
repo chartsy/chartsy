4e638e02dcfadd03d7bbccb6b194045b99c48aa3
viogheba <viogheba@7741bc21-d5e8-4718-ad21-8ba3422b8f82>
2009-12-02 17:21:01 +0000

    - bollinger bands properties and node
    git-svn-id: https://chartsy.svn.sourceforge.net/svnroot/chartsy/trunk@41 7741bc21-d5e8-4718-ad21-8ba3422b8f82

--- Chartsy/Bollinger Bands/src/org/chartsy/bollingerbands/BollingerBands.java
+++ Chartsy/Bollinger Bands/src/org/chartsy/bollingerbands/BollingerBands.java
@@ -11,12 +11,8 @@ import org.chartsy.main.chartsy.DefaultPainter;
 import org.chartsy.main.chartsy.chart.Overlay;
 import org.chartsy.main.dataset.DataItem;
 import org.chartsy.main.dataset.Dataset;
-import org.chartsy.main.utils.ComponentGenerator;
-import org.chartsy.main.utils.Properties;
-import org.chartsy.main.utils.PropertyItem;
 import org.chartsy.main.utils.Range;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
+import org.openide.nodes.AbstractNode;
 
 /**
  *
@@ -25,30 +21,15 @@ import org.w3c.dom.Element;
 public class BollingerBands extends Overlay implements Serializable {
 
     private static final long serialVersionUID = 101L;
-
-    public static final String STD = "# x Std. Dev.";
-    public static final String PERIOD = "Period";
-    public static final String PRICE = "Price";
-    public static final String LABEL = "Label";
-    public static final String MARKER = "Marker";
-    public static final String LOWER_COLOR = "Lower Band Color";
-    public static final String LOWER_STYLE = "Lower Band Line Style";
-    public static final String MIDDLE_COLOR = "Middle Band Color";
-    public static final String MIDDLE_STYLE = "Middle Band Line Style";
-    public static final String UPPER_COLOR = "Upper Band Color";
-    public static final String UPPER_STYLE = "Upper Band Line Style";
-    public static final String INSIDE_COLOR = "Inside Fill";
-    public static final String INSIDE_ALPHA = "Inside Alpha";
     public static final String UPPER = "upper";
     public static final String MIDDLE = "middle";
     public static final String LOWER = "lower";
 
+    private OverlayProperties properties = new OverlayProperties();
+
     public BollingerBands() { super("Bollinger Bands", "Description", "Bollinger"); }
 
-    public String getLabel() {
-        String price = getStringParam(PRICE); String stddev = getStringParam(STD); String period = getStringParam(PERIOD); String label = getStringParam(LABEL);
-        return label + "(" + price + ", " + stddev + ", " + period + ")";
-    }
+    public String getLabel() { return properties.getLabel() + " (" + properties.getPrice() + ", " + properties.getStd() + ", " + properties.getPeriod() + ")"; }
 
     public LinkedHashMap getHTML(ChartFrame cf, int i) {
         LinkedHashMap ht = new LinkedHashMap();
@@ -70,7 +51,7 @@ public class BollingerBands extends Overlay implements Serializable {
     }
 
     public Range getRange(ChartFrame cf) {
-        String price = getStringParam(PRICE);
+        String price = properties.getPrice();
         Dataset upper = visibleDataset(cf, UPPER);
         Dataset lower = visibleDataset(cf, LOWER);
         if (upper != null && lower != null) {
@@ -81,29 +62,9 @@ public class BollingerBands extends Overlay implements Serializable {
         return null;
     }
 
-    public void initialize() {
-        PropertyItem[] data = new PropertyItem[] {
-            new PropertyItem(STD, ComponentGenerator.JTEXTFIELD, "2"),
-            new PropertyItem(PERIOD, ComponentGenerator.JTEXTFIELD, "20"),
-            new PropertyItem(PRICE, ComponentGenerator.JCOMBOBOX, Dataset.LIST, Dataset.CLOSE),
-            new PropertyItem(LABEL, ComponentGenerator.JTEXTFIELD, "Bollinger"),
-            new PropertyItem(MARKER, ComponentGenerator.JCHECKBOX, Boolean.TRUE),
-            new PropertyItem(LOWER_COLOR, ComponentGenerator.JLABEL, new Color(0x204a87)),
-            new PropertyItem(LOWER_STYLE, ComponentGenerator.JSTROKECOMBOBOX, "0"),
-            new PropertyItem(MIDDLE_COLOR, ComponentGenerator.JLABEL, new Color(0x204a87)),
-            new PropertyItem(MIDDLE_STYLE, ComponentGenerator.JSTROKECOMBOBOX, "0"),
-            new PropertyItem(UPPER_COLOR, ComponentGenerator.JLABEL, new Color(0x204a87)),
-            new PropertyItem(UPPER_STYLE, ComponentGenerator.JSTROKECOMBOBOX, "0"),
-            new PropertyItem(INSIDE_COLOR, ComponentGenerator.JLABEL, new Color(0x204a87)),
-            new PropertyItem(INSIDE_ALPHA, ComponentGenerator.JSLIDER, "25")
-        };
-        Properties p = new Properties(data);
-        setProperties(p);
-    }
-
     public void calculate() {
-        int period = getIntParam(PERIOD);
-        int stddev = getIntParam(STD);
+        int period = properties.getPeriod();
+        int stddev = properties.getStd();
         Dataset initial = getDataset();
         if (initial != null && !initial.isEmpty()) {
             Dataset middle = initial.getSMA(period); addDataset(MIDDLE, middle);
@@ -118,15 +79,13 @@ public class BollingerBands extends Overlay implements Serializable {
         Dataset lower = visibleDataset(cf, LOWER);
 
         if (middle != null && upper != null && lower != null) {
-            String price = getStringParam(PRICE);
-            DefaultPainter.insideFill(g, cf, upper, lower, getTransparentColor(getColorParam(INSIDE_COLOR), getIntParam(INSIDE_ALPHA)), price); // paint inside fill
-            DefaultPainter.line(g, cf, middle, getColorParam(MIDDLE_COLOR), getStrokeParam(MIDDLE_STYLE), price); // paint middle line
-            DefaultPainter.line(g, cf, upper, getColorParam(UPPER_COLOR), getStrokeParam(UPPER_STYLE), price); // paint upper line
-            DefaultPainter.line(g, cf, lower, getColorParam(LOWER_COLOR), getStrokeParam(LOWER_STYLE), price); // paint lower line
+            String price = properties.getPrice();
+            if (properties.getInsideVisibility()) DefaultPainter.insideFill(g, cf, upper, lower, properties.getInsideTransparentColor(), price); // paint inside fill
+            DefaultPainter.line(g, cf, middle, properties.getMiddleColor(), properties.getMiddleStroke(), price); // paint middle line
+            DefaultPainter.line(g, cf, upper, properties.getUpperColor(), properties.getUpperStroke(), price); // paint upper line
+            DefaultPainter.line(g, cf, lower, properties.getLowerColor(), properties.getLowerStroke(), price); // paint lower line
         }
     }
-
-    private Color getTransparentColor(final Color color, final int alpha) { return new Color(color.getRed(), color.getGreen(), color.getBlue(), alpha); }
     
     public Dataset getLowerUpperDataset(final Dataset initial, final Dataset middle, final int period, final int stddev, final String type) {
         Vector<DataItem> items = new Vector<DataItem>();
@@ -158,13 +117,13 @@ public class BollingerBands extends Overlay implements Serializable {
         return new Dataset(data);
     }
 
-    public Color[] getColors() { return new Color[] {getColorParam(UPPER_COLOR), getColorParam(MIDDLE_COLOR), getColorParam(LOWER_COLOR)}; }
+    public Color[] getColors() { return new Color[] {properties.getUpperColor(), properties.getMiddleColor(), properties.getLowerColor()}; }
     public double[] getValues(ChartFrame cf) {
         Dataset middle = visibleDataset(cf, MIDDLE);
         Dataset upper = visibleDataset(cf, UPPER);
         Dataset lower = visibleDataset(cf, LOWER);
         if (middle != null && upper != null && lower != null) {
-            String price = (String) getProperties().getValue("Price");
+            String price = properties.getPrice();
             return new double[] {upper.getLastPriceValue(price), middle.getLastPriceValue(price), lower.getLastPriceValue(price)};
         }
         return new double[] {};
@@ -174,14 +133,13 @@ public class BollingerBands extends Overlay implements Serializable {
         Dataset upper = visibleDataset(cf, UPPER);
         Dataset lower = visibleDataset(cf, LOWER);
         if (middle != null && upper != null && lower != null) {
-            String price = (String) getProperties().getValue("Price");
+            String price = properties.getPrice();
             return new double[] {upper.getPriceValue(i, price), middle.getPriceValue(i, price), lower.getPriceValue(i, price)};
         }
         return new double[] {};
     }
-    public boolean getMarkerVisibility() { return getBooleanParam(MARKER); }
+    public boolean getMarkerVisibility() { return properties.getMarker(); }
 
-    public void readXMLDocument(Element parent) { readFromXMLDocument(parent); }
-    public void writeXMLDocument(Document document, Element parent) { writeToXMLDocument(document, parent, "Bollinger Bands"); }
+    public AbstractNode getNode() { return new OverlayNode(properties); }
 
 }
