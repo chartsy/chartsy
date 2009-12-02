d87e1389ddbeb8729974346aca0cb985ebd98842
viogheba <viogheba@7741bc21-d5e8-4718-ad21-8ba3422b8f82>
2009-12-02 17:21:30 +0000

    - ema properties and node
    git-svn-id: https://chartsy.svn.sourceforge.net/svnroot/chartsy/trunk@42 7741bc21-d5e8-4718-ad21-8ba3422b8f82

--- Chartsy/Exponential Moving Average/src/org/chartsy/ema/EMA.java
+++ Chartsy/Exponential Moving Average/src/org/chartsy/ema/EMA.java
@@ -9,12 +9,8 @@ import org.chartsy.main.chartsy.ChartFrame;
 import org.chartsy.main.chartsy.DefaultPainter;
 import org.chartsy.main.chartsy.chart.Overlay;
 import org.chartsy.main.dataset.Dataset;
-import org.chartsy.main.utils.ComponentGenerator;
-import org.chartsy.main.utils.Properties;
-import org.chartsy.main.utils.PropertyItem;
 import org.chartsy.main.utils.Range;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
+import org.openide.nodes.AbstractNode;
 
 /**
  *
@@ -23,25 +19,13 @@ import org.w3c.dom.Element;
 public class EMA extends Overlay implements Serializable {
 
     private static final long serialVersionUID = 101L;
-
-    public static final String PERIOD = "Period";
-    public static final String PRICE = "Price";
-    public static final String LABEL = "Label";
-    public static final String MARKER = "Marker";
-    public static final String COLOR = "Color";
-    public static final String STYLE = "Style";
     public static final String EMA = "ema";
+
+    private OverlayProperties properties = new OverlayProperties();
 
     public EMA() { super("EMA", "Description", "EMA"); }
 
-    public String getLabel() {
-        if (getProperties() == null) {
-            return getDialogLabel();
-        } else {
-            String period = getStringParam(PERIOD); String price = getStringParam(PRICE); String label = getStringParam(LABEL);
-            return label + "(" + price + ", " + period + ")";
-        }
-    }
+    public String getLabel() { return properties.getLabel() + " (" + properties.getPrice() + ", " + properties.getPeriod() + ")"; }
 
     public LinkedHashMap getHTML(ChartFrame cf, int i) {
         LinkedHashMap ht = new LinkedHashMap();
@@ -62,7 +46,7 @@ public class EMA extends Overlay implements Serializable {
     }
 
     public Range getRange(ChartFrame cf) {
-        String price = getStringParam(PRICE);
+        String price = properties.getPrice();
         Dataset ema = visibleDataset(cf, EMA);
         if (ema != null) {
             Range chartRange = cf.getChartRenderer().getChartRange();
@@ -72,23 +56,10 @@ public class EMA extends Overlay implements Serializable {
         return null;
     }
 
-    public void initialize() {
-        PropertyItem[] data = new PropertyItem[] {
-            new PropertyItem(PERIOD, ComponentGenerator.JTEXTFIELD, "20"),
-            new PropertyItem(PRICE, ComponentGenerator.JCOMBOBOX, Dataset.LIST, Dataset.CLOSE),
-            new PropertyItem(LABEL, ComponentGenerator.JTEXTFIELD, "EMA"),
-            new PropertyItem(MARKER, ComponentGenerator.JCHECKBOX, Boolean.TRUE),
-            new PropertyItem(COLOR, ComponentGenerator.JLABEL, new Color(0x204a87)),
-            new PropertyItem(STYLE, ComponentGenerator.JSTROKECOMBOBOX, "0")
-        };
-        Properties p = new Properties(data);
-        setProperties(p);
-    }
-
     public void calculate() {
         Dataset initial = getDataset();
         if (initial != null && !initial.isEmpty()) {
-            int period = getIntParam(PERIOD);
+            int period = properties.getPeriod();
             Dataset ema = initial.getEMA(period);
             addDataset(EMA, ema);
         }
@@ -96,14 +67,14 @@ public class EMA extends Overlay implements Serializable {
 
     public void paint(Graphics2D g, ChartFrame cf) {
         Dataset ema = visibleDataset(cf, EMA);
-        if (ema != null) DefaultPainter.line(g, cf, ema, getColorParam(COLOR), getStrokeParam(STYLE), getStringParam(PRICE)); // paint sma line
+        if (ema != null) DefaultPainter.line(g, cf, ema, properties.getColor(), properties.getStroke(), properties.getPrice()); // paint sma line
     }
 
-    public Color[] getColors() { return new Color[] {getColorParam(COLOR)}; }
+    public Color[] getColors() { return new Color[] {properties.getColor()}; }
     public double[] getValues(ChartFrame cf) {
         Dataset ema = visibleDataset(cf, EMA);
         if (ema != null) {
-            String price = getStringParam(PRICE);
+            String price = properties.getPrice();
             return new double[] {ema.getLastPriceValue(price)};
         }
         return new double[] {};
@@ -111,14 +82,13 @@ public class EMA extends Overlay implements Serializable {
     public double[] getValues(ChartFrame cf, int i) {
         Dataset ema = visibleDataset(cf, EMA);
         if (ema != null) {
-            String price = getStringParam(PRICE);
+            String price = properties.getPrice();
             return new double[] {ema.getPriceValue(i, price)};
         }
         return new double[] {};
     }
-    public boolean getMarkerVisibility() { return getBooleanParam(MARKER); }
+    public boolean getMarkerVisibility() { return properties.getMarker(); }
 
-    public void readXMLDocument(Element parent) { readFromXMLDocument(parent); }
-    public void writeXMLDocument(Document document, Element parent) { writeToXMLDocument(document, parent, "EMA"); }
+    public AbstractNode getNode() { return new OverlayNode(properties); }
 
 }
