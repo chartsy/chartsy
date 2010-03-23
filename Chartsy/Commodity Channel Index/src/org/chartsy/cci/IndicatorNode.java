34932a9edd4e26a12c7d5dc9e0f9d61f5c0997df
viogheba <viogheba@7741bc21-d5e8-4718-ad21-8ba3422b8f82>
2010-03-23 16:35:57 +0000

    New Commodity Channel Index Indicator
    git-svn-id: https://chartsy.svn.sourceforge.net/svnroot/chartsy/trunk@138 7741bc21-d5e8-4718-ad21-8ba3422b8f82

---
+++ Chartsy/Commodity Channel Index/src/org/chartsy/cci/IndicatorNode.java
@@ -0,0 +1,120 @@
+package org.chartsy.cci;
+
+import java.awt.Color;
+import java.awt.Stroke;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+import java.lang.reflect.InvocationTargetException;
+import org.chartsy.main.managers.LoggerManager;
+import org.chartsy.main.utils.StrokeGenerator;
+import org.chartsy.main.utils.StrokePropertyEditor;
+import org.openide.nodes.AbstractNode;
+import org.openide.nodes.Children;
+import org.openide.nodes.PropertySupport;
+import org.openide.nodes.Sheet;
+import org.openide.util.lookup.Lookups;
+
+/**
+ *
+ * @author viorel.gheba
+ */
+public class IndicatorNode extends AbstractNode implements PropertyChangeListener, Externalizable {
+
+    private static final long serialVersionUID = 101L;
+
+    public IndicatorNode() {
+        super(Children.LEAF);
+        setDisplayName("CCI Properties");
+    }
+
+    public IndicatorNode(IndicatorProperties indicatorProperties) {
+        super(Children.LEAF, Lookups.singleton(indicatorProperties));
+        setDisplayName("CCI Properties");
+        indicatorProperties.addPropertyChangeListener(this);
+    }
+
+    @Override
+    protected Sheet createSheet() {
+        Sheet sheet = Sheet.createDefault();
+
+        Sheet.Set set = Sheet.createPropertiesSet();
+        final IndicatorProperties indicatorProperties = getLookup().lookup(IndicatorProperties.class);
+
+        try {
+            // Period
+            @SuppressWarnings(value = "unchecked")
+            Property period = new PropertySupport.Reflection(indicatorProperties, int.class, "getPeriod", "setPeriod") {
+                public Object getValue() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException { return super.getValue(); }
+                public void setValue(Object obj) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException { super.setValue(obj); }
+                public void restoreDefaultValue() throws IllegalAccessException, InvocationTargetException { super.setValue(IndicatorProperties.PERIOD); }
+                public boolean supportsDefaultValue() { return true; }
+            };
+            period.setName("Period");
+            set.put(period);
+
+            // Label
+            @SuppressWarnings(value = "unchecked")
+            Property label = new PropertySupport.Reflection(indicatorProperties, String.class, "getLabel", "setLabel") {
+                public Object getValue() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException { return super.getValue(); }
+                public void setValue(Object obj) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException { super.setValue(obj); }
+                public void restoreDefaultValue() throws IllegalAccessException, InvocationTargetException { super.setValue(IndicatorProperties.LABEL); }
+                public boolean supportsDefaultValue() { return true; }
+            };
+            label.setName("Label");
+            set.put(label);
+
+            // Marker
+            @SuppressWarnings(value = "unchecked")
+            Property marker = new PropertySupport.Reflection(indicatorProperties, boolean.class, "getMarker", "setMarker") {
+                public Object getValue() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException { return super.getValue(); }
+                public void setValue(Object obj) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException { super.setValue(obj); }
+                public void restoreDefaultValue() throws IllegalAccessException, InvocationTargetException { super.setValue(IndicatorProperties.MARKER); }
+                public boolean supportsDefaultValue() { return true; }
+            };
+            marker.setName("Marker");
+            set.put(marker);
+
+            // Color
+            @SuppressWarnings(value = "unchecked")
+            Property color = new PropertySupport.Reflection(indicatorProperties, Color.class, "getColor", "setColor") {
+                public Object getValue() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException { return super.getValue(); }
+                public void setValue(Object obj) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException { super.setValue(obj); }
+                public void restoreDefaultValue() throws IllegalAccessException, InvocationTargetException { super.setValue(IndicatorProperties.COLOR); }
+                public boolean supportsDefaultValue() { return true; }
+            };
+            color.setName("Signal Color");
+            set.put(color);
+
+            // Stroke
+            @SuppressWarnings(value = "unchecked")
+            PropertySupport.Reflection stroke = new PropertySupport.Reflection(indicatorProperties, Stroke.class, "getStroke", "setStroke") {
+                public Object getValue() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException { return super.getValue(); }
+                public void setValue(Object obj) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException { super.setValue(obj); }
+                public void restoreDefaultValue() throws IllegalAccessException, InvocationTargetException { super.setValue(StrokeGenerator.getStroke(IndicatorProperties.STROKE_INDEX)); }
+                public boolean supportsDefaultValue() { return true; }
+            };
+            stroke.setPropertyEditorClass(StrokePropertyEditor.class);
+            stroke.setName("Line Style");
+            set.put(stroke);
+        } catch (Exception e) {
+            LoggerManager.getDefault().log(e.getMessage(), e);
+        }
+
+        sheet.put(set);
+
+        return sheet;
+    }
+
+    public void propertyChange(PropertyChangeEvent evt) {
+        firePropertySetsChange(null, getPropertySets());
+    }
+
+    public void writeExternal(ObjectOutput out) throws IOException {}
+
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {}
+
+}
---
+++ Chartsy/Commodity Channel Index/src/org/chartsy/cci/IndicatorProperties.java
@@ -0,0 +1,69 @@
+package org.chartsy.cci;
+
+import java.awt.Color;
+import java.awt.Stroke;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import org.chartsy.main.utils.StrokeGenerator;
+
+/**
+ *
+ * @author viorel.gheba
+ */
+public class IndicatorProperties implements Serializable {
+
+    private static final long serialVersionUID = 101L;
+
+    public static final int PERIOD = 14;
+    public static final String LABEL = "CCI";
+    public static final boolean MARKER = true;
+    public static final Color COLOR = new Color(0x4e9a06);
+    public static int STROKE_INDEX = 0;
+
+    private int period = PERIOD;
+    private String label = LABEL;
+    private boolean marker = MARKER;
+    private Color color = COLOR;
+    private int strokeIndex = STROKE_INDEX;
+
+    public IndicatorProperties() {}
+
+    public int getPeriod() { return period; }
+    public void setPeriod(int i) { period = i; }
+
+    public String getLabel() { return label; }
+    public void setLabel(String s) { label = s; }
+
+    public boolean getMarker() { return marker; }
+    public void setMarker(boolean b) { marker = b; }
+
+    public Color getColor() { return color; }
+    public void setColor(Color c) { color = c; }
+
+    public int getStrokeIndex() { return strokeIndex; }
+    public void setStrokeIndex(int i) { strokeIndex = i; }
+    public Stroke getStroke() { return StrokeGenerator.getStroke(strokeIndex); }
+    public void setStroke(Stroke s) { strokeIndex = StrokeGenerator.getStrokeIndex(s); }
+
+    private List listeners = Collections.synchronizedList(new LinkedList());
+
+    public void addPropertyChangeListener(PropertyChangeListener pcl) {
+        listeners.add(pcl);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener pcl) {
+        listeners.remove(pcl);
+    }
+
+    private void fire(String propertyName, Object old, Object nue) {
+        PropertyChangeListener[] pcls = (PropertyChangeListener[]) listeners.toArray(new PropertyChangeListener[0]);
+        for (int i = 0; i < pcls.length; i++) {
+            pcls[i].propertyChange(new PropertyChangeEvent(this, propertyName, old, nue));
+        }
+    }
+
+}
