8e70f0f158b4bd05508142389833297349a36021
viogheba <viogheba@7741bc21-d5e8-4718-ad21-8ba3422b8f82>
2010-08-25 12:18:46 +0000

    StockScanPRO final release - bugs fixed
    git-svn-id: https://chartsy.svn.sourceforge.net/svnroot/chartsy/trunk@286 7741bc21-d5e8-4718-ad21-8ba3422b8f82

---
+++ Chartsy/StockScanPRO/src/org/chartsy/stockscanpro/actions/EmailScansAction.java
@@ -0,0 +1,25 @@
+package org.chartsy.stockscanpro.actions;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Viorel
+ */
+public class EmailScansAction implements ActionListener
+{
+
+	private static final Logger LOG = Logger.getLogger(
+		EmailScansAction.class.getPackage().getName());
+
+	public EmailScansAction()
+	{}
+
+	public void actionPerformed(ActionEvent e)
+	{
+		// to do
+	}
+
+}
