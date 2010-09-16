5b8db127adc65c0ca7030ac3fd2065d622428e20
nlitnd1isaloof <nlitnd1isaloof@7741bc21-d5e8-4718-ad21-8ba3422b8f82>
2010-09-16 02:05:22 +0000

    finally fixed linear regression module issues and It is now an overlay as it is supposed to be.
    git-svn-id: https://chartsy.svn.sourceforge.net/svnroot/chartsy/trunk@308 7741bc21-d5e8-4718-ad21-8ba3422b8f82

---
+++ Chartsy/Linear Regression/src/org/chartsy/linreg/OverlayProperties.java
@@ -0,0 +1,61 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.chartsy.linreg;
+
+import java.awt.Color;
+import java.awt.Stroke;
+import org.chartsy.main.chart.AbstractPropertyListener;
+import org.chartsy.main.data.Dataset;
+import org.chartsy.main.utils.ColorGenerator;
+import org.chartsy.main.utils.SerialVersion;
+import org.chartsy.main.utils.StrokeGenerator;
+
+/**
+ *
+ * @author Joshua.taylor
+ */
+public class OverlayProperties extends AbstractPropertyListener
+{
+
+    private static final long serialVersionUID = SerialVersion.APPVERSION;
+
+    public static final int PERIOD = 20;
+    public static final String PRICE = Dataset.CLOSE;
+    public static final String LABEL = "Linear Regression";
+    public static final boolean MARKER = true;
+    public static Color COLOR;
+    public static final int STROKE_INDEX = 0;
+
+    private int period = PERIOD;
+    private String price = PRICE;
+    private String label = LABEL;
+    private boolean marker = MARKER;
+    private Color color;
+    private int strokeIndex = STROKE_INDEX;
+
+    public OverlayProperties() { COLOR = ColorGenerator.getRandomColor(); color = COLOR; }
+
+    public int getPeriod() { return period; }
+    public void setPeriod(int i) { period = i; }
+
+    public String getPrice() { return price; }
+    public void setPrice(String s) { price = s; }
+
+    public String getLabel() { return label; }
+    public void setLabel(String s) { label = s; }
+
+    public boolean getMarker() { return marker; }
+    public void setMarker(boolean b) { marker = b; }
+
+    public Color getColor() { return color; }
+    public void setColor(Color c) { color = c; }
+
+    public int getStrokeIndex() { return strokeIndex; }
+    public void setStrokeIndex(int i) { strokeIndex = i; }
+    public Stroke getStroke() { return StrokeGenerator.getStroke(strokeIndex); }
+    public void setStroke(Stroke s) { strokeIndex = StrokeGenerator.getStrokeIndex(s); }
+
+}
